faf28df8afd2ffc580b75e912fd1df68bae52ad2
Arju Srivastava <krishnabhakt677@gmail.com>
2026-07-22 23:16:46 +0530

    Setting up architecture

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    ip : req.ip,
+    message: "RangamAI is responding to your request!"
+
+  })
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
